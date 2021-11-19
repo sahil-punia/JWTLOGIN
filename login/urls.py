047fe6c0586b0d8e27d8624194b2f68d803d7091
sahil punia7 <sahilpuniawins@gmail.com>
2021-11-19 12:25:45 +0530

    user.save()

--- login/urls.py
+++ login/urls.py
@@ -10,7 +10,6 @@ from .views import (
 urlpatterns = [
     path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('register', UserRegistrationView.as_view(), name='register'),
-    path('login', UserLoginView.as_view(), name='login'),
-    # path('users', UserListView.as_view(), name='users')
+    path('api/register', UserRegistrationView.as_view(), name='register'),
+    path('api/login', UserLoginView.as_view(), name='login'),
 ]
